94595691c85c746a5a3aabfae640ca1a81332ba0
stidl <m.stidl@gmx.net>
2022-06-01 18:48:07 +0200

    add read.mjs

---
+++ js/read.mjs
@@ -0,0 +1,26 @@
+const FilePath = "js.json";
+
+/**
+ * @returns {Promise<Question[]>} The questions
+ */
+async function readQuestions() {
+    /**
+     * @type {Object[]}
+     */
+    const entries = await fetch(FilePath).then(res=>res.json());
+
+
+return entries.map(entry=>({
+    id: entry.id,
+    text: entry.question,
+    answers: [
+        { text: entry.a, isCorrect: entry.answer === 'a'},
+        { text: entry.b, isCorrect: entry.answer === 'b'},
+        { text: entry.c, isCorrect: entry.answer === 'c'},
+        { text: entry.d, isCorrect: entry.answer === 'd'},
+        { text: entry.e, isCorrect: entry.answer === 'e'},
+    ]
+}));
+}
+
+export default readQuestions;
